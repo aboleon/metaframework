e29a8efb25b9c96bb33b4efc4d04ed8ea3fc0fc0
Andrian Mihailov <dev@aboleon.media>
2025-03-19 12:06:37 +0100

    Update: mfw-messages

--- src/Resources/views/components/response-messages.blade.php
+++ src/Resources/views/components/response-messages.blade.php
@@ -1,4 +1,4 @@
-<div class="messages">
+<div class="messages" id="mfw-messages">
     {!! MetaFramework\Accessors\ResponseParser::parseResponse(session('session_response')) !!}
     @php
     session()->forget('session_response');
