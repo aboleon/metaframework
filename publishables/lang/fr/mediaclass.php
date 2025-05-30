9753307e695ec3aad70f3c587ee29efc7754cf5c
Andrian Mihailov <dev@aboleon.media>
2025-05-30 18:08:02 +0200

    Update: Mediaclass translatable

--- publishables/lang/fr/mediaclass.php
+++ publishables/lang/fr/mediaclass.php
@@ -1,23 +1,25 @@
 <?php
 
 return [
-    'buttons' => [
-        'send' => 'Envoyer',
-        'save' => 'Enregistrer',
+    'buttons'      => [
+        'send'         => 'Envoyer',
+        'save'         => 'Enregistrer',
         'select_image' => 'Choisir une image',
-        'download' => 'Télécharger',
-        'cancel' => 'Annuler',
-        'error' => 'Erreur',
-        'select' => 'Sélectionner'
+        'download'     => 'Télécharger',
+        'cancel'       => 'Annuler',
+        'error'        => 'Erreur',
+        'select'       => 'Sélectionner',
     ],
-    'file' => 'fichier|fichiers',
-    'uploaded_at' => "Téléchargé le :date à :time",
-    'errors' => [
-        'missing_model' => "Le média doit obligatoirement appartenir à un objet",
-        'mustBeImage' => "Le fichier n'est pas une image",
+    'file'         => 'fichier|fichiers',
+    'uploaded_at'  => "Téléchargé le :date à :time",
+    'errors'       => [
+        'missing_model'    => "Le média doit obligatoirement appartenir à un objet",
+        'mustBeImage'      => "Le fichier n'est pas une image",
         'maxNumberOfFiles' => "Le nombre maxium de fichiers que vous pouvez télécharger est ",
-        'maxFileSize' => "Le fichier est trop volumineux. Poids maximum : "
+        'maxFileSize'      => "Le fichier est trop volumineux. Poids maximum : ",
+        'dimensions'       => "L'image doit avoir une taille minimale de :width x :height pixels. Image uploadée : :uploaded_width x :uploaded_height pixels.",
     ],
-    'no_media' => "Aucun média n'a été ajouté",
+    'no_media'     => "Aucun média n'a été ajouté",
     'no_documents' => "Aucun document n'a été ajouté",
 ];
+
