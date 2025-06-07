60dcedb32e96328affe1cd32407359d586b74be5
Andrian Mihailov <dev@aboleon.media>
2025-06-07 22:46:14 +0200

    Update: radio component

--- src/Resources/views/components/radio.blade.php
+++ src/Resources/views/components/radio.blade.php
@@ -4,6 +4,7 @@
     @endif
     @forelse($values as $value => $title)
         <x-mfw::input-radio :affected="$affected"
+                            :default="$default"
                             :value="$value"
                             :name="$name"
                             :label="str_starts_with($title, 'trans.') ? trans(str_replace('trans.','', $title)) : $title"
