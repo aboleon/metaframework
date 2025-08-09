9ac1ad520671cf6f926ddb0a9ee56ac2eed1bbba
Andrian Mihailov <dev@aboleon.media>
2025-08-09 16:47:25 +0200

    Update: QueryLocale trait

---
+++ src/Traits/Models/QueryLocale.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MetaFramework\Traits\Models;
+
+trait QueryLocale
+{
+    private ?string $query_locale = null;
+
+    public function setQueryLocale(string $query_locale): self
+    {
+        $this->query_locale = $query_locale;
+
+        return $this;
+    }
+
+    public function getQueryLocale(): string
+    {
+        if ( ! $this->query_locale) {
+            $this->setQueryLocale(app()->getFallbackLocale());
+        }
+
+        return $this->query_locale;
+    }
+}
