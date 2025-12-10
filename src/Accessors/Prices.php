d8c80ffc8d992da500d7b6ac60b61ca710d7cbac
Andrian Mihailov <dev@aboleon.media>
2025-12-10 13:22:18 +0100

    Prices: accept null, 0 as default

--- src/Accessors/Prices.php
+++ src/Accessors/Prices.php
@@ -2,21 +2,17 @@
 
 namespace MetaFramework\Accessors;
 
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Cache;
-use MetaFramework\Models\Vat;
-
 class Prices
 {
     public static function fromInteger(int $price): int|float
     {
-        return $price/100;
+        return $price / 100;
     }
 
     public static function toInteger(int|float|string $price): int
     {
         if (is_string($price)) {
-            $price = floatval(str_replace(',','.', $price));
+            $price = floatval(str_replace(',', '.', $price));
         }
         $price = $price * 100;
 
@@ -24,19 +20,20 @@ class Prices
     }
 
     public static function readableFormat(
-        int|float $price, string $currency = '€',
+        null|int|float $price = 0,
+        string $currency = '€',
         string $decimal_separator = ',',
         string $thousand_separator = ' ',
-        bool $showDecimals = true): string
-    {
+        bool $showDecimals = true,
+    ): string {
         $formatted_price = number_format($price, 2, $decimal_separator, $thousand_separator);
 
-        list($integer_part, $decimal_part) = explode($decimal_separator, $formatted_price);
+        [$integer_part, $decimal_part] = explode($decimal_separator, $formatted_price);
 
         if ($showDecimals) {
-            return rtrim($formatted_price . ' ' . $currency);
+            return rtrim($formatted_price.' '.$currency);
         }
 
-        return rtrim($integer_part . ' ' . $currency);
+        return rtrim($integer_part.' '.$currency);
     }
 }
