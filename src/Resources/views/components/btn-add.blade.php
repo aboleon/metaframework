85a481ae0aa7c58e4673c2fa288e232d407bcb10
Andrian Mihailov <dev@aboleon.media>
2026-01-12 00:07:09 +0100

    Update: Add wrapper to btn components

--- src/Resources/views/components/btn-add.blade.php
+++ src/Resources/views/components/btn-add.blade.php
@@ -1,7 +1,7 @@
 @props([
-    'route' => null,
+    'route' => null
 ])
-<a class="btn btn-success"
+<a class="btn btn-success text-nowrap"
    id="mfw-add-button" {!! $route ? 'href="'.$route.'"' : '' !!}>
-    <i class="bi bi-plus-lg"></i> {{ __('mfw.add') }}
+    <i class="bi bi-plus-lg"></i>&nbsp;<span class="btn-text">{{ __('mfw.add') }}</span>
 </a>
