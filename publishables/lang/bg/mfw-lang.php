d52a9ffb1c86f8947cdf572bb4a26e24c671ab85
Andrian Mihailov <dev@aboleon.media>
2026-02-03 23:17:58 +0100

    Add: Go Back button

---
+++ publishables/lang/bg/mfw-lang.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+return [
+    'fr' => [
+        'label' => 'Френски',
+        'label_original' => 'Français',
+        'code' => 'fr',
+    ],
+    'en' => [
+        'label' => 'Английски',
+        'label_original' => 'English',
+        'code' => 'en',
+    ],
+    'bg' => [
+        'label' => 'Български',
+        'label_original' => 'Български',
+        'code' => 'bg',
+    ],
+    'de'=> [
+        'label' => 'Немски',
+        'label_original' => 'Deutsch',
+        'code' => 'de'
+    ],
+
+];
---
+++ src/Resources/views/components/btn-back.blade.php
@@ -0,0 +1,5 @@
+@if (url()->previous() !== request()->fullUrl())
+    <a class="btn btn-outline-secondary text-nowrap" href="{!! url()->previous() !!}">
+        <i class="bi bi-chevron-double-left"></i>&nbsp;<span class="btn-text">{!! __('mfw.goback') !!}</span>
+    </a>
+@endif
