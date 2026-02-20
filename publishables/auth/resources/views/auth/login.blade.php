724b81316304e3c8feb0b47b90b9d3098e6f1711
Andrian Mihailov <dev@aboleon.media>
2026-02-20 18:48:38 +0100

    Fix: publishable auth layout

--- publishables/auth/resources/views/auth/login.blade.php
+++ publishables/auth/resources/views/auth/login.blade.php
@@ -1,65 +1,48 @@
-<x-guest-layout>
-
+<x-auth-layout>
     @push('css')
         {!!  csscrush_tag(public_path('front/css/auth.css')) !!}
     @endpush
-
-    @if(is_file(public_path('media/logo.png')))
-        <a href="/">
-            <img src="{{ url('media/logo.png') }}" class="logo" alt="{{ config('app.name') }}" style="max-width: 70px">
-        </a>
-    @endif
-
-    @if ($errors->any())
-        <div>
-            @foreach ($errors->all() as $error)
-                <x-mfw-support::alert type="warning"
-                              :message="__(str_replace('passwords.', 'auth.password.forgotten.',$error))"/>
-            @endforeach
-        </div>
-    @endif
-
     <!-- Session Status -->
     <x-mfw::auth-session-status class="mb-4" :status="session('status')"/>
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <div class="row">
-            <div class="col-2 offset-4">
-                <div>
-                    <x-mfw-inputable::input type="email"
-                                  :label="__('mfw-auth.email')"
-                                  name="email" :value="old('email')"
-                                  :required="true"/>
-                </div>
-
-                <div class="mt-4">
-                    <x-mfw-inputable::input type="password"
-                                  :label="__('mfw-auth.password.label')"
-                                  name="password" :value="old('email')"
-                                  :required="true"
-                                  :params="['autocomplete'=>'current-password']"
-                    />
-                </div>
-
-                <x-mfw-inputable::checkbox name="remember" :label="__('mfw-auth.keepMe')" value="1" :affected="old('remember')"/>
-
-
-                <div class="d-flex align-items-center justify-content-between mt-4">
-                    @if (Route::has('password.request'))
-                        <a href="{{ route('password.request') }}">
-                            {{ __('mfw-auth.password.forgotten.label') }}
-                        </a>
-                    @endif
-
-                    <button class="btn btn-sm btn-dark">
-                        {{ __('mfw-auth.loginBtn') }}
-                    </button>
-                </div>
+        @if(is_file(public_path('media/logo.png')))
+            <div class="text-center">
+                <img src="{{ url('media/logo.png') }}" alt="{{ config('app.name') }}"  class="logo rounded-3 mb-5 w-50">
             </div>
+        @endif
+
+        <div>
+            <x-mfw-inputable::input type="email"
+                                    :label="__('mfw-auth.email')"
+                                    name="email" :value="old('email')"
+                                    :required="true"/>
+        </div>
+
+        <div class="my-4">
+            <x-mfw-inputable::input type="password"
+                                    :label="__('mfw-auth.password.label')"
+                                    name="password" :value="old('email')"
+                                    :required="true"
+                                    :params="['autocomplete'=>'current-password']"
+            />
+        </div>
+
+        <x-mfw-inputable::checkbox name="remember" :label="__('mfw-auth.keepMe')" value="1" :affected="old('remember')"/>
+
+
+        <div class="d-flex align-items-center justify-content-between mt-4">
+            @if (Route::has('password.request'))
+                <a href="{{ route('password.request') }}">
+                    {{ __('mfw-auth.password.forgotten.label') }}
+                </a>
+            @endif
+
+            <button class="btn btn-sm btn-dark">
+                {{ __('mfw-auth.loginBtn') }}
+            </button>
         </div>
     </form>
-
-
-</x-guest-layout>
+</x-auth-layout>
---
+++ publishables/auth/resources/views/layouts/auth.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.css">
+    {!! csscrush_tag(public_path('css/panel.css')) !!}
+    @stack('css')
+
+</head>
+<body style="background-color: #f8fafc">
+<div class="d-flex vh-100 justify-content-center align-items-center">
+    <main>
+        {{ $slot }}
+    </main>
+</div>
+</body>
+</html>
